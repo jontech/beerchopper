6690e170eaf5b7da675eede6dec5bf731cb69dc9
Jonas Andriunaitis <andriunaitis@gmail.com>
2020-01-27 17:08:47 +0200

    add routing model with chepest path heuristic and solution print method

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -8,8 +8,21 @@ import java.sql.ResultSet;
 import java.util.function.Function;
 import java.util.ArrayList;
 import java.util.List;
+import com.google.ortools.constraintsolver.Assignment;
+import com.google.ortools.constraintsolver.FirstSolutionStrategy;
+import com.google.ortools.constraintsolver.RoutingIndexManager;
+import com.google.ortools.constraintsolver.RoutingModel;
+import com.google.ortools.constraintsolver.RoutingSearchParameters;
+import com.google.ortools.constraintsolver.main;
+import java.util.logging.Logger;
 
 public class App {
+    static {
+        System.loadLibrary("jniortools");
+    }
+
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+    
     public static void main(String[] args) {
         Connection conn = null;
         Statement stmt = null;
@@ -30,7 +43,50 @@ public class App {
             return;
         }
 
-        DataModel data = new DataModel(geoLocations);
-        System.out.println(data.transMat.length);
+        final DataModel data = new DataModel(geoLocations);
+
+        RoutingIndexManager manager =
+            new RoutingIndexManager(data.transMat.length, data.vehicleNumber, data.depot);
+
+        RoutingModel routing = new RoutingModel(manager);
+
+        final int transitCallbackIndex =
+            routing.registerTransitCallback((long fromIndex, long toIndex) -> {
+                    // Convert from routing variable Index to user NodeIndex.
+                    int fromNode = manager.indexToNode(fromIndex);
+                    int toNode = manager.indexToNode(toIndex);
+                    return data.transMat[fromNode][toNode];
+                });
+
+        routing.setArcCostEvaluatorOfAllVehicles(transitCallbackIndex);
+
+        RoutingSearchParameters searchParameters =
+            main.defaultRoutingSearchParameters()
+            .toBuilder()
+            .setFirstSolutionStrategy(FirstSolutionStrategy.Value.PATH_CHEAPEST_ARC)
+            .build();
+
+        Assignment solution = routing.solveWithParameters(searchParameters);
+
+        printSolution(routing, manager, solution);
     }
+
+    static void printSolution(RoutingModel routing, RoutingIndexManager manager, Assignment solution) {
+        // Solution cost.
+        logger.info("Objective: " + solution.objectiveValue() + "miles");
+        // Inspect solution.
+        logger.info("Route:");
+        long routeDistance = 0;
+        String route = "";
+        long index = routing.start(0);
+        while (!routing.isEnd(index)) {
+            route += manager.indexToNode(index) + " -> ";
+            long previousIndex = index;
+            index = solution.value(routing.nextVar(index));
+            routeDistance += routing.getArcCostForVehicle(previousIndex, index, 0);
+        }
+        route += manager.indexToNode(routing.end(0));
+        logger.info(route);
+        logger.info("Route distance: " + routeDistance + "km");
+    }    
 }
