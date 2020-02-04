ca15c84fc6096b5c0d849527b4eef8b848ffa9bd
Jonas Andriunaitis <andriunaitis@gmail.com>
2020-02-04 14:58:24 +0200

    use db bean in db tests

--- src/test/java/project/DBTest.java
+++ src/test/java/project/DBTest.java
@@ -5,19 +5,24 @@ import static org.junit.Assert.*;
 import java.util.List;
 import java.util.Arrays;
 import java.sql.SQLException;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runner.RunWith;
 
+@RunWith(SpringRunner.class)
 public class DBTest {
+    @Autowired private DB db;
+
     @Test public void testGetGeoLocations() throws SQLException {
-        DB db = new DB("jdbc:mysql://db/beerchopper?user=root&password=123");
-        List<GeoLocation> geoLocations = db.getGeoLocations();
+        List<GeoLocation> geoLocations = this.db.getGeoLocations();
 
         assertEquals(1304, geoLocations.size());
     }
 
     @Test public void testGetBreweriesByIds() throws SQLException {
-        DB db = new DB("jdbc:mysql://db/beerchopper?user=root&password=123");
         List<Integer> ids = Arrays.asList(1, 2, 3);
-        List<Brewery> breweries = db.getBreweries(ids);
+        List<Brewery> breweries = this.db.getBreweries(ids);
 
         assertEquals(3, breweries.size());
     }
